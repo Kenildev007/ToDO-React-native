4b075ccde547d124f362f23838684d9429784c41
Kenildev007 <kenilgorasya6@gmail.com>
2024-02-15 12:14:37 +0530

    all done

--- src/screens/TodoCompleted.jsx
+++ src/screens/TodoCompleted.jsx
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const TodoCompleted = ({ navigation, completedTasks }) => {
@@ -8,11 +8,14 @@ const TodoCompleted = ({ navigation, completedTasks }) => {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <View style={styles.background}>
                 <Text style={styles.header}>Completed Tasks</Text>
-                {completedTasks.map((elem, index) => (
-                    <View key={index}>
-                        <Text>{elem.text}</Text>
-                    </View>
-                ))}
+
+                <ScrollView>
+                    {completedTasks.map((elem, index) => (
+                        <View style={styles.completedText} key={index}>
+                            <Text style={styles.completeColor}>{elem.text}</Text>
+                        </View>
+                    ))}
+                </ScrollView>
             </View>
             <Button title="Go Back" onPress={() => navigation.goBack()} />
         </View>
@@ -25,6 +28,7 @@ export default TodoCompleted;
 const styles = StyleSheet.create({
     header: {
         fontSize: 20,
+        width: 380,
         textAlign: 'center',
         margin: 10,
         color: '#333333',
@@ -32,5 +36,17 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: '#C9D7DD',
+    },
+    completedText: {
+        flexDirection: 'row',
+        backgroundColor: 'green',
+        margin: 10,
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#F0F3FF',
+    },
+    completeColor: {
+        color: 'white',
     }
 })
